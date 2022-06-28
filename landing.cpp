fd85142129bcdbf24968746957b4ae5db60aa148
iago <iagosilvestrehp@hotmail.com>
2022-06-28 14:35:27 -0300

    fixed old landing

--- landing.cpp
+++ landing.cpp
@@ -6,21 +6,31 @@ voltar a posição inicial, numa distribuição circular com 4 posicoes.
 Quando completa 4 posicoes de pouso adiciona um offset para os proximos 4,
 e assim em diante.
 */
+#include <iostream>
+#include <vector>
+
 int uavN=getUavNumber(); //Assumindo que uavNumber comeca em 1
-float landZeroX=-109.0;
-float landZeroY=-121.0;
+float landZeroX=-102.0;
+float landZeroY=-111.0;
 
 float landingX,landingY;
+std::vector <int> landingPos (2);
 
 int lane=(uavN/4)+1; // Divisao inteira por 4, inicia em 1
 int pos=uavN%4; // Resto da divisao por 4
-int modQuad; // Quadrante angular em relacao ao origem da area de pouso
+int modQuad=1; // Quadrante angular em relacao ao origem da area de pouso
 
+if(pos==3 || pos==0){
+  modQuad=-1;
+}
 if(pos==1 || pos==3){
   landingX=landZeroX;
-  landingY=landZeroY+(1.5*lane);
+  landingY=landZeroY+(1.5*lane*modQuad); //Para quadrante positivo adiciona, negativo subtrai
 }
 else{ //(pos==2 || pos==0)
-  landingX=landZeroX+(1.5*lane);
+  landingX=landZeroX+(1.5*lane*modQuad); //Para quadrante positivo adiciona, negativo subtrai
   landingY=landZeroY;
 }
+landingPos[0]=landingX;
+landingPos[1]=landingY;
+return landingPos;
